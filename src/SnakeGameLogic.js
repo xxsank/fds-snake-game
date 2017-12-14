302905df5a217551ecd8343534db1e0bc794dfb2
Kim Seungha <seungha.me@gmail.com>
2017-12-14 11:50:30 +0900

    프로토타입 수정

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -2,36 +2,43 @@ import {ROWS, COLS} from './config';
 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [{x: 1, y: 0}, {x: 0, y: 0}];
+  this.joints = [
+    {x: 2, y: 10},
+    {x: 1, y: 0},
+    {x: 0, y: 0},
+    {x: 10, y: 7}
+  ];
 
   // 먹이의 좌표
-  this.fruit = {x: 5, y: 10};
+  this.fruit = {x: 3, y: 5};
 }
 
-SnakeGameLogic.prototype = {
-  up() {
-    // 위쪽 화살표 키를 누르면 실행되는 함수
-    console.log('up');
-  },
-  down() {
-    // 아래쪽 화살표 키를 누르면 실행되는 함수
-    console.log('down');
-  },
-  left() {
-    // 왼쪽 화살표 키를 누르면 실행되는 함수
-    console.log('left');
-  },
-  right() {
-    // 오른쪽 화살표 키를 누르면 실행되는 함수
-    console.log('right');
-  },
-  nextState() {
-    // 한 번 움직여야 할 타이밍마다 실행되는 함수
+SnakeGameLogic.prototype.up = function() {
+  // 위쪽 화살표 키를 누르면 실행되는 함수
+  console.log('up');
+}
+
+SnakeGameLogic.prototype.down = function() {
+  // 아래쪽 화살표 키를 누르면 실행되는 함수
+  console.log('down');
+}
+
+SnakeGameLogic.prototype.left = function() {
+  // 왼쪽 화살표 키를 누르면 실행되는 함수
+  console.log('left');
+}
+
+SnakeGameLogic.prototype.right = function() {
+  // 오른쪽 화살표 키를 누르면 실행되는 함수
+  console.log('right');
+}
+
+SnakeGameLogic.prototype.nextState = function() {
+  // 한 번 움직여야 할 타이밍마다 실행되는 함수
     // 게임이 아직 끝나지 않았으면 `true`를 반환
     // 게임이 끝났으면 `false`를 반환
     console.log(`nextState`);
     return true;
-  },
 }
 
 export default SnakeGameLogic;
